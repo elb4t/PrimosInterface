14341944e38d9539281331f242d0478b35c0d9c6
Elb4t <eloy4t@gmail.com>
2018-02-19 20:19:41 +0100

    Implementar la tarea asíncrona en clase independiente

--- app/src/main/java/es/elb4t/primosinterfaces/MainActivity.kt
+++ app/src/main/java/es/elb4t/primosinterfaces/MainActivity.kt
@@ -8,10 +8,11 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskListener {
     companion object {
         private val TAG = MainActivity::class.java.name
     }
+
     private var inputField: EditText? = null
     private lateinit var resultField: EditText
     private lateinit var primecheckbutton: Button
@@ -26,16 +27,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun triggerPrimecheck(v: View) {
-        if (mAsyncTask!!.status == AsyncTask.Status.RUNNING) {
+        if (mAsyncTask == null)
+            mAsyncTask = MyAsyncTask(this)
+        if (mAsyncTask!!.status !== AsyncTask.Status.RUNNING) {
             Log.v(TAG, "Thread " + Thread.currentThread().id + ": triggerPrimecheck() comienza")
             val parameter = java.lang.Long.parseLong(inputField!!.text.toString())
-            mAsyncTask = MyAsyncTask()
+            mAsyncTask = MyAsyncTask(this)
             mAsyncTask!!.execute(parameter)
             Log.v(TAG, "Thread " + Thread.currentThread().id + ": triggerPrimecheck() termina")
         } else {
             Log.v(TAG, "Cancelando test " + Thread.currentThread().id)
             mAsyncTask!!.cancel(true)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 
     override fun onStop() {
@@ -45,53 +53,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private inner class MyAsyncTask : AsyncTask<Long, Double, Boolean>() {
-        override fun doInBackground(vararg n: Long?): Boolean? {
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": doInBackground() starts")
-            val numComprobar = n[0]
-            if (numComprobar!! < 2 || numComprobar % 2 == 0L)
-                return false
-            val limit = Math.sqrt(numComprobar.toDouble()) + 0.0001
-            var progress = 0.0
-            var factor: Long = 3
-            while (factor < limit && !isCancelled) {
-                if (numComprobar % factor == 0L)
-                    return false
-
-                if (factor > limit * progress / 100) {
-                    publishProgress(progress / 100)
-                    progress += 5.0
-                }
-                factor += 2
-            }
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": doInBackground() ends")
-            return true
-        }
-
-        override fun onPreExecute() {
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": onPreExecute()")
-            resultField!!.setText("")
-            primecheckbutton!!.text = "CANCELAR"
-        }
-
-        override fun onProgressUpdate(vararg progress: Double?) {
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": onProgressUpdate()")
-            resultField!!.setText(String.format("%.1f%% completed", progress[0]!! * 100))
-        }
-
-        override fun onPostExecute(isPrime: Boolean?) {
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": onPostExecute()")
-            resultField!!.setText(isPrime!!.toString())
-            primecheckbutton.text = "¿ES PRIMO?"
-        }
-
-        override fun onCancelled() {
-            Log.v(TAG, "Thread " + Thread.currentThread().id + ": onCancelled")
-            super.onCancelled()
-            resultField.setText("Proceso cancelado")
-            primecheckbutton.text = "¿ES PRIMO?"
-        }
+    override fun onPreExecute() {
+        resultField.setText("")
+        primecheckbutton.text = "CANCELAR"
     }
 
+    override fun onProgressUpdate(progreso: Double?) {
+        resultField.setText(String.format("%.1f%% completado", progreso!! * 100))
+    }
+
+    override fun onPostExecute(resultado: Boolean) {
+        resultField.setText(resultado.toString() + "")
+        primecheckbutton.text = "¿ES PRIMO?"
+    }
+
+    override fun onCancelled() {
+        resultField.setText("Proceso cancelado")
+        primecheckbutton.text = "¿ES PRIMO?"
+    }
 
 }
